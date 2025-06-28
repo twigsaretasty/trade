24e9a46584f635742ba89a57c0bc54362b90433d
Twigs <twigsaretasty@gmail.com>
2025-06-27 22:32:41 -0700

    content cleanup and misc fixes

--- home.js
+++ home.js
@@ -1,1 +1,52 @@
 console.log("Trading home page.");
+
+function getDays(timestamp) {
+    const date = new Date(timestamp);
+    const now = new Date();
+
+    const utcDate = Date.UTC(date.getFullYear(), date.getMonth(), date.getDate());
+    const utcNow = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate());
+
+    const diffDays = Math.floor((utcNow - utcDate) / (1000 * 60 * 60 * 24));
+
+    if (diffDays === 0) {
+        return "today";
+    } else if (diffDays === 1) {
+        return "1 day ago";
+    } else {
+        return diffDays + " days ago";
+    }
+}
+
+fetch('profile.json')
+    .then(response => response.json())
+    .then(data => {
+        const profile = data.profile;
+        const slug = data.slug;
+        const username = data.username;
+        const name = data.name;
+        const pronouns = data.pronouns;
+        const email = data.email;
+        const status = data.status;
+        const lastSeen = data.last_seen;
+        const recordingsCount = data.recordings;
+        const wantsCount = data.wants;
+
+        // Set status
+        document.getElementById('status').textContent = status;
+
+        // Apply respective styles for status
+        if (status === "open") {
+            document.getElementById('status').classList.add('status-open');
+        }
+        else if (status === "closed") {
+            document.getElementById('status').classList.add('status-closed');
+        }
+        else if (status === "limited") {
+            document.getElementById('status').classList.add('status-limited');
+        }
+
+        // Get last seen days
+        const lastSeenString = getDays(lastSeen);
+        document.getElementById('seen').textContent = lastSeenString;
+    })
